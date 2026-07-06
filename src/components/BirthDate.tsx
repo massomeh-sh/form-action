0284148f37802bca64d5983e78d3435d058e22cc
Massomeh <massomeh4790@gmail.com>
2026-07-06 19:26:38 +0330

    fix netlify build

--- src/components/BirthDate.tsx
+++ src/components/BirthDate.tsx
@@ -13,18 +13,22 @@ function BirthDate({errorMessage}: BirthDateProps): JSX.Element {
     const maxBirthDate = new Date();
     maxBirthDate.setFullYear(maxBirthDate.getFullYear() - 18);
     return (
-        <div className="flex flex-col md:gap-1 relative">
+        <div className="flex flex-col md:gap-1">
             <label htmlFor="birthday" className="text-xs md:text-sm font-semibold text-gray-700"
             >Birthdate</label>
-            <DatePicker name="birthday" id="birthday" selected={birthday}
-                        onChange={(date: Date | null) => setBirthday(date)} placeholderText="Select your birthday"
-                        dateFormat="dd/MM/yyy" showYearDropdown
-                        showMonthDropdown scrollableYearDropdown yearDropdownItemNumber={80}
-                        maxDate={maxBirthDate}
-                        className="w-full outline-none px-2 py-1.5 text-[8px] md:text-[10px] border-2 border-gray-200 focus:border-gray-400 rounded-lg"
+            <div className="relative">
+                <DatePicker name="birthday" id="birthday" selected={birthday}
+                            onChange={(date: Date | null) => setBirthday(date)} placeholderText="Select your birthday"
+                            dateFormat="dd/MM/yyy" showYearDropdown
+                            showMonthDropdown scrollableYearDropdown yearDropdownItemNumber={80}
+                            maxDate={maxBirthDate}
+                            wrapperClassName="w-full"
+                            className="w-full outline-none px-2 py-1.5 text-[8px] md:text-[10px] border-2 border-gray-200 focus:border-gray-400 rounded-lg"
 
-            />
-            <IoCalendarOutline className="absolute top-6.5  right-2 text-gray-500 pointer-events-none"/>
+                />
+                <IoCalendarOutline
+                    className="absolute top-1/2 -translate-y-1/2 right-2 text-gray-500 pointer-events-none"/>
+            </div>
             {errorMessage && <ErrorMessage errorMessage={errorMessage}/>}
         </div>
     );
